1f90cf175bca4cfe8e4d2b735581b3ef07e74b5f
Daniel Cousens <github@dcousens.com>
2016-08-31 17:25:53 +1000

    use ES5 for tests too

--- test/index.js
+++ test/index.js
@@ -4,13 +4,13 @@ var tape = require('tape')
 
 fixtures.forEach(function (f) {
   tape(f.description, function (t) {
-    var outputs = f.outputs.map(value => ({ value }))
-    var unspents = f.unspents.map((value, i) => ({ i, value }))
+    var outputs = f.outputs.map(function (value) { return { value } })
+    var unspents = f.unspents.map(function (value, i) { return { i, value } })
     var result = coinSelect(unspents, outputs, f.feeRate)
 
     // drop non-index related input data for easy result comparison
     if (result.inputs) {
-      result.inputs = result.inputs.map(input => input.i)
+      result.inputs = result.inputs.map(function (input) { return input.i })
     }
 
     t.deepEqual(result, f.expected)
