2b2e0c71ee3a988fbcec8bd5f5536b11a3a9f807
Daniel Cousens <github@dcousens.com>
2016-09-03 16:13:22 +1000

    tests: fix standard is undefined

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 let coinSelect = require('../')
-let fixtures = require('./fixtures/standard')
+let fixtures = require('./fixtures')
 let tape = require('tape')
 
 fixtures.forEach((f) => {
   tape(f.description, (t) => {
     let inputs = f.inputs.map((x, i) => x.value ? Object.assign({ i }, x) : { i, value: x })
     let outputs = f.outputs.map(x => x.script ? x : { value: x })
-    let result = coinSelect.standard(inputs, outputs, f.feeRate)
+    let result = coinSelect(inputs, outputs, f.feeRate)
 
     // drop non-index related input data for easy result comparison
     if (result.inputs) {
