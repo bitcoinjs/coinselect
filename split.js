64431cbd64b69d60d2429b5ecd4463f67ac8e971
Daniel Cousens <github@dcousens.com>
2016-10-12 19:43:06 +1100

    split: add further tests for pre-defined outputs

--- split.js
+++ split.js
@@ -6,23 +6,25 @@ module.exports = function split (utxos, outputs, feeRate) {
   const fee = bytesAccum * feeRate
   if (outputs.length === 0) return { fee }
 
-  const remaining = inAccum - fee
+  let outAccum = outputs.reduce(function (a, x) { return a + (x.value | 0) }, 0)
+  const remaining = inAccum - outAccum - fee
   if (remaining <= 0) return { fee }
 
-  const valuePerOutput = (remaining / outputs.length) | 0
-  if (valuePerOutput <= utils.dustThreshold({}, feeRate)) {
-    return { fee }
-  }
+  const wildOutputsCount = outputs.reduce(function (a, x) {
+    return a + (x.value === undefined)
+  }, 0)
+  const wildValue = (remaining / wildOutputsCount) | 0
 
-  outputs = outputs.map(function (output) {
-    return Object.assign({
-      value: valuePerOutput
-    }, output)
+  if (!outputs.every(function (x) {
+    if (x.value !== undefined) return true
+
+    return wildValue > utils.dustThreshold(x, feeRate)
+  })) return { fee }
+
+  outputs = outputs.map(function (x) {
+    return Object.assign({ value: wildValue }, x)
   })
 
-  const outAccum = outputs.reduce(function (a, x) { return a + x.value }, 0)
-  const actualFee = inAccum - outAccum
-  if (actualFee < fee) return { fee }
-
+  const actualFee = inAccum - outputs.reduce(function (a, x) { return a + x.value }, 0)
   return { inputs: utxos, outputs, fee: actualFee }
 }
--- test/split.js
+++ test/split.js
@@ -5,14 +5,19 @@ let tape = require('tape')
 fixtures.forEach((f, k) => {
   tape(f.description, (t) => {
     let inputs = f.inputs.map((x, i) => x.value ? x : { value: x })
-    let outputs = []
-    for (var i = 0; i < f.outputs; ++i) outputs.push({})
+    let outputs = f.outputs
+
+    // avoid useless information in the fixtures
+    if (typeof outputs === 'number') {
+      outputs = []
+      for (var i = 0; i < f.outputs; ++i) outputs.push({})
+    }
 
     let result = coinSplit(inputs, outputs, f.feeRate)
 
     // ensure arguments were not modified
     t.equal(inputs.length, f.inputs.length)
-    t.equal(outputs.length, f.outputs)
+    t.equal(outputs.length, f.outputs.length || f.outputs)
 
     // drop non-index related input data for easy result comparison
     if (result.inputs) {
