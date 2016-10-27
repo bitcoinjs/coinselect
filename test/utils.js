a36e2da1eff5db234e3513fa449d35208556ae23
Daniel Cousens <github@dcousens.com>
2016-10-27 11:17:33 +1100

    tests: add utils tests

---
+++ test/utils.js
@@ -0,0 +1,19 @@
+var tape = require('tape')
+var utils = require('../utils')
+
+tape('utils', function (t) {
+  t.test('uintOrNaN', function (t) {
+    t.plan(8)
+
+    t.equal(utils.uintOrNaN(1), 1)
+    t.equal(isNaN(utils.uintOrNaN('')), true)
+    t.equal(isNaN(utils.uintOrNaN(Infinity)), true)
+    t.equal(isNaN(utils.uintOrNaN(NaN)), true)
+    t.equal(isNaN(utils.uintOrNaN('1')), true)
+    t.equal(isNaN(utils.uintOrNaN('1.1')), true)
+    t.equal(isNaN(utils.uintOrNaN(1.1)), true)
+    t.equal(isNaN(utils.uintOrNaN(-1)), true)
+  })
+
+  t.end()
+})
