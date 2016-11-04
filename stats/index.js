9581ccc3bb059c3e11300b20a944eaa78d8c245e
Daniel Cousens <github@dcousens.com>
2016-11-04 14:41:14 +1100

    add result merging option

--- stats/index.js
+++ stats/index.js
@@ -7,7 +7,7 @@ let results = []
 let utxos = Simulation.generateTxos(15, min, max)
 
 // n samples
-for (var j = 0; j < 100; ++j) {
+for (var j = 0; j < 1000; ++j) {
   // for each strategy
   for (var name in modules) {
     let f = modules[name]
@@ -31,17 +31,41 @@ function pad (i) {
   return ('          ' + i).slice(-10)
 }
 
+function merge (results) {
+  let resultMap = {}
+
+  results.forEach(x => {
+    let { stats } = x
+    let result = resultMap[stats.name]
+
+    if (result) {
+      result.inputs += stats.inputs
+      result.outputs += stats.outputs
+      result.transactions += stats.transactions
+      result.failed += stats.failed
+      result.fees += stats.fees
+      result.bytes += stats.bytes
+    } else {
+      resultMap[stats.name] = Object.assign({}, stats)
+    }
+  })
+
+  return Object.keys(resultMap).map(k => ({ stats: resultMap[k] }))
+}
+
 // sort by transactions ASCENDING
 // then fees DESCENDING
 // top 20 only
-results.sort((a, b) => {
-  if (a.stats.transactions !== a.stats.transactions) return a.stats.transactions - b.stats.transactions
-  return b.stats.fees - a.stats.fees
+merge(results).sort((a, b) => {
+  let aDNF = a.stats.failed / (a.stats.failed + a.stats.transactions)
+  let bDNF = b.stats.failed / (b.stats.failed + b.stats.transactions)
+
+  return aDNF - bDNF
 }).slice(0, 20).forEach(x => {
   let { stats } = x
   let nInputs = stats.inputs / stats.transactions
   let nOutputs = stats.outputs / stats.transactions
-  let failedRatio = stats.failed / (stats.failed + stats.transactions)
+  let DNF = stats.failed / (stats.failed + stats.transactions)
   let feeRateAverage = Math.floor(stats.fees / stats.bytes)
   let feeAverage = Math.floor(stats.fees / stats.transactions)
 
@@ -51,6 +75,6 @@ results.sort((a, b) => {
     '| feeRate', pad('' + feeRateAverage),
     '| nInputs', pad(nInputs),
     '| nOutputs', pad(nOutputs),
-    '| DNF', Math.round(100 * failedRatio) + '%'
+    '| DNF', Math.round(100 * DNF) + '%'
   )
 })
