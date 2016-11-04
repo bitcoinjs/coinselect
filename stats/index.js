2c8cbbd51883f26b2c30538ec3c4520dfec5d9ca
Daniel Cousens <github@dcousens.com>
2016-11-04 13:52:59 +1100

    add cleaner simulation code

--- stats/index.js
+++ stats/index.js
@@ -1,24 +1,4 @@
-function generate (target, range) {
-  let arr = []
-
-  while (target > 0) {
-    let value = Math.floor(Math.random() * range)
-    target -= value
-    arr.push({ value })
-  }
-
-  return arr
-}
-
-function pad (i) {
-  if (typeof i === 'number') {
-    i = Math.round(i * 1000) / 1000
-  }
-
-  return ('          ' + i).slice(-10)
-}
-
-//
+let Simulation = require('./simulation')
 let strategies = [
   'bestof', 'maximal', 'minimal', 'random', 'proximal', // plain accumulative
   'blackmax', 'blackmin', 'blackrand', // blackjack & accumulative
@@ -26,97 +6,47 @@ let strategies = [
 ]
 
 let modules = strategies.map(name => ({ name, f: require('./' + name) }))
-
-function evaluate (algorithm, utxos, targets, feeRate) {
-  const nTransactions = targets.length
-  let cnc = targets.length
-  let feeAccum = 0
-  let nOutputs = 0
-  let nInputs = 0
-  let o = 0
-
-  let utxoMap = {}
-
-  function addUtxo (txo) {
-    if (txo.external) return
-    utxoMap[o] = { id: o, value: txo.value }
-
-    ++o
-    ++nOutputs
-  }
-
-  utxos.forEach(addUtxo)
-
-  targets.forEach((target) => {
-    let nowUtxos = []
-    for (var id in utxoMap) {
-      nowUtxos.push(utxoMap[id])
-    }
-
-    let { inputs, outputs, fee } = algorithm(nowUtxos, [target], feeRate)
-    if (!inputs) return
-    feeAccum += fee
-    --cnc
-
-    inputs.forEach(x => {
-      ++nInputs
-      delete utxoMap[x.id]
-    })
-    outputs.forEach(addUtxo)
-  })
-
-  return { feeAccum, nTransactions, nInputs, nOutputs, cnc }
-}
-
-let resultMap = {}
-
-for (var m = 1; m < 5; ++m) {
-  for (var l = 1; l < 3; ++l) {
-    for (var k = 1; k < 3; ++k) {
-      let feeRate = 56 / l
-      let amount = 300000000 // 3BTC
-      let utxos = generate(amount, 20000000 * k) // 0.2BTC max
-      let outputs = generate(amount, 2000000 / m) // spend ~54USD max
-      outputs.forEach(x => (x.external = true))
-
-      console.log('UTXOS', utxos.length, 'Transactions', outputs.length)
-
-      modules.forEach(({ name, f }) => {
-        let x = evaluate(f, utxos, outputs, feeRate)
-
-        if (!resultMap[name]) {
-          resultMap[name] = x
-          return
-        }
-
-        let y = resultMap[name]
-        y.feeAccum += x.feeAccum
-        y.nTransactions += x.nTransactions
-        y.nInputs += x.nInputs
-        y.nOutputs += x.nOutputs
-        y.cnc += x.cnc
-      })
-    }
-  }
-}
+let min = 14226 // 0.1 USD
+let max = 142251558 // 1000 USD
 
 let results = []
-for (let name in resultMap) {
-  let x = resultMap[name]
+modules.forEach(({ name, f }) => {
+  let simulation = new Simulation(name, f, 56)
+  let utxos = Simulation.generateTxos(1000, min * 10, max * 10)
+  utxos.forEach(x => simulation.addUTXO(x))
 
-  x.name = name
-  x.nTC = x.nTransactions - x.cnc
-  x.nIT = x.nInputs / x.nTC
-  x.nOT = x.nOutputs / x.nTC
-  x.DNF = x.cnc / x.nTransactions
+  // 100 rounds
+  for (let i = 0; i < 100; ++i) {
+    let outputs = Simulation.generateTxos(1, min, max / 4)
+    outputs.forEach(x => (x.address = 'A'))
 
-  results.push(x)
-}
+    simulation.run(outputs)
+  }
 
-results.sort((a, b) => {
-  return b.feeAccum - a.feeAccum
+  results.push(simulation)
 })
 
-results.forEach(x => {
-  console.log(pad(x.name), '| fA', pad('' + x.feeAccum), '| NIT', pad(x.nIT), '| NOT', pad(x.nOT), '| DNF', Math.round(100 * x.DNF) + '%')
+function pad (i) {
+  if (typeof i === 'number') i = Math.round(i * 1000) / 1000
+  return ('          ' + i).slice(-10)
+}
+
+// sort by transactions ASCENDING
+// then fees DESCENDING
+results.sort((a, b) => {
+  if (a.stats.transactions !== a.stats.transactions) return a.stats.transactions - b.stats.transactions
+  return b.stats.fees - a.stats.fees
+}).forEach(x => {
+  let { stats } = x
+  let nIT = stats.inputs / stats.transactions
+  let nOT = stats.outputs / stats.transactions
+  let fT = stats.failed / stats.transactions
+
+  console.log(
+    pad(stats.name),
+    '| fA', pad('' + stats.fees),
+    '| NIT', pad(nIT),
+    '| NOT', pad(nOT),
+    '| DNF', Math.round(100 * fT) + '%'
+  )
 })
---
+++ stats/simulation.js
@@ -0,0 +1,77 @@
+function rayleight (a, b) {
+  return a + b * Math.sqrt(-Math.log(uniform(0, 1)))
+}
+
+function uniform (min, max) {
+  return min + (max - min) * Math.random()
+}
+
+function Simulation (name, algorithm, feeRate) {
+  this.algorithm = algorithm
+  this.feeRate = feeRate
+  this.utxoMap = {}
+  this.stats = {
+    name,
+    transactions: 0,
+    inputs: 0,
+    outputs: 0,
+    fees: 0,
+    failed: 0
+  }
+
+  // used for tracking UTXOs (w/o transaction ids)
+  this.k = 0
+}
+
+Simulation.generateTxos = function (n, min, max) {
+  let txos = []
+  for (let i = 0; i < n; ++i) {
+    let v = rayleight(min, max) >>> 0
+    txos.push({ value: v })
+  }
+  return txos
+}
+
+Simulation.prototype.addUTXO = function (utxo) {
+  let k = this.k + 1
+  if (this.utxoMap[k] !== undefined) throw new Error('Bad UTXO')
+
+  this.utxoMap[k] = Object.assign({}, utxo, { id: k })
+  this.k = k
+}
+
+Simulation.prototype.useUTXO = function (utxo) {
+  if (utxo.id === undefined) throw new Error('Bad UTXO')
+  if (this.utxoMap[utxo.id] === undefined) throw new Error('Unknown UTXO')
+  delete this.utxoMap[utxo.id]
+}
+
+Simulation.prototype.getUTXOs = function () {
+  let utxos = []
+  for (let k in this.utxoMap) utxos.push(this.utxoMap[k])
+  return utxos
+}
+
+Simulation.prototype.run = function (outputs) {
+  let utxos = this.getUTXOs()
+
+  let { inputs, outputs: outputs2, fee } = this.algorithm(utxos, outputs, this.feeRate)
+
+  if (!inputs) {
+    this.stats.failed += 1
+    return
+  }
+
+  this.stats.transactions += 1
+  this.stats.inputs += inputs.length
+  this.stats.outputs += outputs2.length
+  this.stats.fees += fee
+
+  inputs.forEach(x => this.useUTXO(x))
+
+  // selected outputs w/ no address are change addresses, add them to the UTXO
+  outputs2.filter(x => x.address === undefined).forEach(x => this.addUTXO(x))
+  return true
+}
+
+module.exports = Simulation
