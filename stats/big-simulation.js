a79ba33bd9b8c380c8819ec81a403671586e44c8
Karel Bilek <kb@karelbilek.com>
2017-07-12 18:50:13 +0200

    Adding moneypot simulation

---
+++ stats/big-simulation.js
@@ -0,0 +1,37 @@
+let Simulation = require('./simulation')
+let modules = require('./strategies')
+let feeRate = 56 * 100
+let results = []
+
+let list = require('./moneypot-hotwallet.json')
+
+// for each strategy
+for (var name in modules) {
+  let f = modules[name]
+  let simulation = new Simulation(name, f, feeRate)
+
+  console.log(name)
+  let i = 0
+  for (var value of list) {
+    i++
+
+    if (i % 1000 === 0) {
+      console.log(i)
+    }
+    let txo = {
+      address: 'A',
+      value: Math.abs(value)
+    }
+
+    if (value > 0) {
+      simulation.addUTXO(txo)
+    } else {
+      simulation.run([txo])
+    }
+  }
+
+  simulation.finish()
+  results.push(simulation)
+}
+
+Simulation.printResults(Simulation.mergeResults(results))
