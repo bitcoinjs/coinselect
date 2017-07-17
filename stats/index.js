817ce8ce6cbbe9bc6de7c50203801da25059f0c0
Karel Bilek <kb@karelbilek.com>
2017-07-17 17:19:52 +0200

    Using input and output sizes from statistics

--- stats/index.js
+++ stats/index.js
@@ -5,6 +5,42 @@ let max = 142251558 // 1000 USD
 let feeRate = 56 * 100
 let results = []
 
+let walletType = 'p2pkh' // set to either p2pkh or p2sh
+
+// data from blockchaing from ~august 2015 - august 2017
+// see https://gist.github.com/runn1ng/8e2881e5bb44e01748e46b3d1c549038
+// these are 2-of-3 lengths, most common p2sh (66%), percs changed so they add to 100
+let scripthashScriptLengthData = {
+  prob: 0.16,
+  inLengthPercs: {
+    252: 25.29,
+    253: 49.77,
+    254: 24.94
+  },
+  outLength: 23
+}
+
+// these are compressed key length (90% of p2pkh inputs)
+// percs changed so they add to 100
+let pubkeyhashScriptLengthData = {
+  prob: 0.84,
+  inLengthPercs: {
+    105: 0.4,
+    106: 50.7,
+    107: 48.81,
+    108: 0.09
+  },
+  outLength: 25
+}
+
+let selectedData = walletType === 'p2pkh' ? pubkeyhashScriptLengthData : scripthashScriptLengthData
+let inLengthProbs = selectedData.inLengthPercs
+
+let outLengthProbs = {};
+[scripthashScriptLengthData, pubkeyhashScriptLengthData].forEach(({prob, outLength}) => {
+  outLengthProbs[outLength] = prob
+})
+
 // n samples
 for (var j = 0; j < 100; ++j) {
   if (j % 200 === 0) console.log('Iteration', j)
@@ -12,9 +48,8 @@ for (var j = 0; j < 100; ++j) {
   let stages = []
 
   for (var i = 1; i < 4; ++i) {
-    let utxos = Simulation.generateTxos(20 / i, min, max)
-    let txos = Simulation.generateTxos(80 / i, min, max / 3)
-
+    let utxos = Simulation.generateTxos(20 / i, min, max, inLengthProbs)
+    let txos = Simulation.generateTxos(80 / i, min, max / 3, outLengthProbs)
     stages.push({ utxos, txos })
   }
 
