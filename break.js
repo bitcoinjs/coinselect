c73f5d126eabbd163fea9effa7c101725e30d47e
Daniel Cousens <dcousens@users.noreply.github.com>
2017-08-16 10:35:04 +1000

    Update break.js

--- break.js
+++ break.js
@@ -1,6 +1,6 @@
 var utils = require('./utils')
 
-// break utxos into the maximum number of output possible
+// break utxos into the maximum number of 'output' possible
 module.exports = function broken (utxos, output, feeRate) {
   if (!isFinite(utils.uintOrNaN(feeRate))) return {}
 
