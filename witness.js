8ebc838110f8919055072f456a5c3ae6ca9004af
Leon <lixiao.dev@gmail.com>
2024-12-04 10:50:46 +0800

    fix: params filter

--- witness.js
+++ witness.js
@@ -7,23 +7,15 @@ module.exports = function coinSelect ({
 	changeAddress,
 	network,
 	dustThreshold = 546,
+	txType,
 }) {
-	const finalOutputs = outputs.map(o => ({
-		...o,
-		amount: o.type === 'opreturn' || o.type === 'send-max' ? undefined : Number(o.value).toString(),
-		type: o.type === 'opreturn' ? 'opreturn' : o.type === 'send-max' ? 'send-max' : 'payment'
-	}))
 
 	const result = composeTx({
-		utxos: utxos.map(u => ({
-			...u,
-			coinbase: false,
-			amount: Number(u.value).toString(),
-		})),
-		outputs: finalOutputs,
-		feeRate: Number(feeRate).toString(), 
+		utxos,
+		outputs,
+		feeRate, 
 		sortingStrategy: 'random',
-		txType: 'p2wpkh',
+		txType,
 		dustThreshold,
 		changeAddress,
 		network
